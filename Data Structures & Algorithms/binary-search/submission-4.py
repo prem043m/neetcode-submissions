d0542311851d2ea12ec2b58ffe0bf148f0c84221
Prem Mahaveer Kamble <premkamble8752@gmail.com>
2026-09-18 21:05:15 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = left + (right-left)//2
+
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        return -1
